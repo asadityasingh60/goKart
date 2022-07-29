0e67b99b923460b3cbed42b38013d320e6703247
Aditya Narayan Singh <asadityasingh60@gmail.com>
2022-07-30 00:32:26 +0530

    d

--- backend/app.js
+++ backend/app.js
@@ -28,11 +28,18 @@ app.use("/api/v1",user);
 app.use("/api/v1/",order);
 app.use("/api/v1/",payment);
 
-app.use(express.static(path.join(__dirname,"../frontend/build")));
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
-});
+__dirname = path.resolve();
+if(process.env.NODE_ENV==="PRODUCTION"){
+    app.use(express.static(path.join(__dirname,"/frontend/build")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+    });
+} 
+
+
+
 
 //MiddleWare for Error
 app.use(errorMiddleware);
